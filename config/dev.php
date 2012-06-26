780f62a69ee1ccf562e3a7fe4b0b5b0f196b0192
Fabien Potencier <fabien.potencier@gmail.com>
2012-06-26 20:12:47 +0200

    added loggin in dev env

--- config/dev.php
+++ config/dev.php
@@ -1,7 +1,13 @@
 <?php
 
+use Silex\Provider\MonologServiceProvider;
+
 // include the prod configuration
 require __DIR__.'/prod.php';
 
 // enable the debug mode
 $app['debug'] = true;
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../silex.log',
+));
