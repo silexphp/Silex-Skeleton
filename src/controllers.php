0f4727928955ce817c943e20f55533b4d8be6bcd
Victor <victor@suumit.com>
2013-02-08 12:22:15 +0100

    [Controllers] Remove uneeded use clauses

--- src/controllers.php
+++ src/controllers.php
@@ -2,9 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html', array());
