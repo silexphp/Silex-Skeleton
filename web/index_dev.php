7b8f6ed75107a68c52927dad83e4b002e44f03b0
Dmitri Perunov <diimpp@gmail.com>
2013-07-01 12:26:46 -0700

    Update index_dev.php to work from localhost only
    Symfony2 has this check for app_dev.php, so I think will be good to have it also.

--- web/index_dev.php
+++ web/index_dev.php
@@ -4,6 +4,16 @@ use Symfony\Component\ClassLoader\DebugClassLoader;
 use Symfony\Component\HttpKernel\Debug\ErrorHandler;
 use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
 
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 error_reporting(-1);
