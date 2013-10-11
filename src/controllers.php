893c4ccb4624e0ebe2c8c3d81f502b76749e6978
Fabien Potencier <fabien.potencier@gmail.com>
2013-10-11 11:06:44 +0200

    added the trusted proxies setting

--- src/controllers.php
+++ src/controllers.php
@@ -6,6 +6,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+//Request::setTrustedProxies(array('127.0.0.1'));
+
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html', array());
 })
