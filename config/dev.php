8065e5571e92413c88ce7a2e9d5f0c52eaac4d13
Gusakov Nikita <dev@nkt.me>
2014-04-25 02:38:13 +0400

    Removed duplicate web-profiler mounting
    https://github.com/silexphp/Silex-WebProfiler/blob/master/WebProfilerServiceProvider.php#L207

--- config/dev.php
+++ config/dev.php
@@ -13,7 +13,6 @@ $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
 ));
 
-$app->register($p = new WebProfilerServiceProvider(), array(
+$app->register(new WebProfilerServiceProvider(), array(
     'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
 ));
-$app->mount('/_profiler', $p);
