62e6a70c747ec55aff2926f13672c8120e3c8566
Fabien Potencier <fabien.potencier@gmail.com>
2014-11-24 09:51:38 +0100

    added HttpFragmentServiceProvider as it is needed by the web profiler

--- src/app.php
+++ src/app.php
@@ -5,12 +5,14 @@ use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
 
 $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
 $app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
