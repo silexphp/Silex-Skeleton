60322e5f4fd32058149fbdd897dc242197167a85
Fabien Potencier <fabien.potencier@gmail.com>
2014-11-24 09:43:50 +0100

    re-added the web profiler

--- config/dev.php
+++ config/dev.php
@@ -1,6 +1,7 @@
 <?php
 
 use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\WebProfilerServiceProvider;
 
 // include the prod configuration
 require __DIR__.'/prod.php';
@@ -10,4 +11,8 @@ $app['debug'] = true;
 
 $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
+));
+
+$app->register(new WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
 ));
