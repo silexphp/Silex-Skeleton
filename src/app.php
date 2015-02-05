7e6438e5898f967652e1af22c1253f3d86ede884
Loïck Piera <pyrech@gmail.com>
2015-02-05 23:10:01 +0100

    Check whether the asset url start with a slash

--- src/app.php
+++ src/app.php
@@ -17,7 +17,7 @@ $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
     $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) use ($app) {
-        return $app['request_stack']->getMasterRequest()->getBasepath().'/'.$asset;
+        return $app['request_stack']->getMasterRequest()->getBasepath().'/'.ltrim($asset, '/');
     }));
 
     return $twig;
