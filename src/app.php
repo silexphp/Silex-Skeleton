a88b05f09b10673aca6aa282adf15c4ca5468055
Fabien Potencier <fabien.potencier@gmail.com>
2015-09-10 20:00:48 +0200

    added the asset service provider by default

--- src/app.php
+++ src/app.php
@@ -1,12 +1,14 @@
 <?php
 
 use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
+$app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
