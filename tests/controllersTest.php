e082024bf0794f6c8771dfeea92f9c6c2968f578
Fabien Potencier <fabien.potencier@gmail.com>
2015-09-10 20:16:33 +0200

    added some more examples in the test example

--- tests/controllersTest.php
+++ tests/controllersTest.php
@@ -4,6 +4,16 @@ use Silex\WebTestCase;
 
 class controllersTest extends WebTestCase
 {
+    public function testGetHomepage()
+    {
+        $client = $this->createClient();
+        $client->followRedirects(true);
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertContains('Welcome', $crawler->filter('body')->text());
+    }
+
     public function createApplication()
     {
         $app = require __DIR__.'/../src/app.php';
@@ -12,13 +22,5 @@ class controllersTest extends WebTestCase
         $app['session.test'] = true;
 
         return $this->app = $app;
-    }
-
-    public function testGetRoot()
-    {
-        $client = $this->createClient();
-        $client->followRedirects(true);
-        $client->request('GET', '/');
-        $this->assertTrue($client->getResponse()->isOk());
     }
 }
