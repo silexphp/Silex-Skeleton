489fe0ad2c8270b03542c2132ad493d84999a64a
Fabien Potencier <fabien.potencier@gmail.com>
2015-09-10 19:58:40 +0200

    remove obsolete code

--- src/app.php
+++ src/app.php
@@ -2,23 +2,15 @@
 
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\RoutingServiceProvider;
-use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 
 $app = new Application();
-$app->register(new RoutingServiceProvider());
-$app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
-
-    $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) use ($app) {
-        return $app['request_stack']->getMasterRequest()->getBasepath().'/'.ltrim($asset, '/');
-    }));
 
     return $twig;
 });
