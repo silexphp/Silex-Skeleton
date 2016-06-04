16edc95648bbf6faf75dd552e79b22d7b9c0375a
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2016-06-04 20:20:29 +0900

    install bootstrap, select2, font-awesome

--- src/app.php
+++ src/app.php
@@ -2,6 +2,7 @@
 
 use Silex\Application;
 use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
@@ -11,6 +12,7 @@ $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+$app->register(new SessionServiceProvider());
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
