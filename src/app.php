d4dcd50dbc6e1852817a9e12e036f1778f228eab
Marius Ghita <mhitza@gmail.com>
2016-10-29 20:59:39 +0300

    Update code and Readme about included Providers
    From the initial README.rst the `ValidatorServiceProvider` was
    missing, and there were already included providers like:
    `HttpFragmentServiceProvider` and `AssetServiceProvider` which
    where not specified in README.rst

--- src/app.php
+++ src/app.php
@@ -5,12 +5,14 @@ use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+$app->register(new ValidatorServiceProvider());
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
